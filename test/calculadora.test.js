f8e2c0b8b2c625e913acb8145afd69fa59eae61d
MarcosProfAds <seu-email@exemplo.com>
2024-11-21 10:43:12 -0300

    primeiro commit

---
+++ test/calculadora.test.js
@@ -0,0 +1,31 @@
+const assert = require("assert");
+const Calculadora = require("../calculadora");
+
+describe("Calculadora", function () {
+  let calc;
+
+  beforeEach(function () {
+    calc = new Calculadora();
+  });
+
+  it("deve somar dois números corretamente", function () {
+    const resultado = calc.somar(5, 3);
+    assert.strictEqual(resultado, 8);
+  });
+
+  it("deve subtrair dois números corretamente", function () {
+    const resultado = calc.subtrair(10, 4);
+    assert.strictEqual(resultado, 6);
+  });
+
+  it("deve multiplicar dois números corretamente", function () {
+    const resultado = calc.multiplicar(3, 7);
+    assert.strictEqual(resultado, 21);
+  });
+
+  it("deve dividir dois números corretamente", function () {
+    const resultado = calc.dividir(15, 3);
+    assert.strictEqual(resultado, 5);
+  });
+
+});
